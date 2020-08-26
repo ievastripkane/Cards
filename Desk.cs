727cfda2632b66e4069f62c74fdc945527577edf
Ieva Stripkāne <ieva.stripkane.2002@gmail.com>
2020-08-26 15:40:58 +0300

    version 15:40

---
+++ Desk.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace Cards
+{
+    public partial class Desk : Form
+    {
+        private string folderPath = null;
+        private string[] fileNames = null;
+        private Random rand = new Random(); 
+        public Desk()
+        {
+            InitializeComponent();
+            InitializeDesk();
+        }
+
+        private void InitializeDesk()
+        {
+            this.BackColor = Color.Green;
+
+        }
+
+        private void LoadCards_Click(object sender, EventArgs e)
+        {
+            PictureBox filePictureBox = null;
+            folderPath = @"C:\Users\Ieva\source\repos\Cards\Playing Cards\Playing Cards\playing_card_images\face";
+            fileNames = Directory.GetFiles(folderPath); 
+
+            foreach(var fileName in fileNames)
+            {
+                filePictureBox = new PictureBox()
+                {
+                    Height = 100,
+                    Width = 70,
+                    SizeMode = PictureBoxSizeMode.StretchImage,
+                    Left = rand.Next(0, 400),
+                    Top = rand.Next(50, 400),
+                    Image = Image.FromFile(fileName)
+                };
+                this.Controls.Add(filePictureBox);
+            }
+        }
+    }
+}
